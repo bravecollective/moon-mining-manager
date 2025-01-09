96470ba08e61c5bdcd0859822ed7afb3f25b5135
JayWood <jjwood2004@gmail.com>
2025-01-08 23:16:35 -0500

    Add filtering to Moon Admin page ( main page )

--- resources/views/moons/list.blade.php
+++ resources/views/moons/list.blade.php
@@ -1,5 +1,65 @@
 @extends('layouts.master')
 
+@php
+    use Faker\Factory as Faker;
+
+    $faker = Faker::create();
+    $moons = collect(range(1, 600))->map(function ($id) use ($faker) {
+        $regions = ['Delve', 'Querious'];
+        $systems = [
+            '1DQ1-A', 'T5ZI-S', 'M2-XFE', 'C3N-3S', '5BTK-M',
+            'D-W7F0', '8QT-H4', 'SVM-3K', 'V-IH6B', 'U9U-TQ'
+        ];
+        $moonGooMinerals = [
+            'Chromium', 'Cobalt', 'Scandium', 'Titanium',
+            'Tungsten', 'Vanadium', 'Cadmium', 'Platinum'
+        ];
+
+		$mineral2 = $faker->boolean;
+		$mineral3 = $faker->boolean;
+		$mineral4 = $faker->boolean;
+
+        $data = [
+            'id' => $id,
+            'region' => (object)['regionName' => $faker->randomElement($regions)],
+            'system' => (object)['solarSystemName' => $faker->randomElement($systems)],
+            'planet' => $faker->numberBetween(1, 10),
+            'moon' => $faker->numberBetween(1, 5),
+            'mineral_1' => (object)['typeName' => $faker->randomElement($moonGooMinerals)],
+            'mineral_1_percent' => $faker->randomFloat(2, 5, 50),
+            'mineral_2_type_id' => $mineral2 ? $faker->randomNumber() : 0,
+            'mineral_2' => $mineral2 ? (object)['typeName' => $faker->randomElement($moonGooMinerals)] : (object)['typeName' => '' ],
+            'mineral_2_percent' => $mineral2 ? $faker->randomFloat(2, 5, 50) : 0,
+            'mineral_3_type_id' => $mineral3 ? $faker->randomNumber() : 0,
+            'mineral_3' => $mineral3 ? (object)['typeName' => $faker->randomElement($moonGooMinerals)] : (object)['typeName' => '' ],
+            'mineral_3_percent' => $mineral3 ? $faker->randomFloat(2, 5, 50) : 0,
+            'mineral_4_type_id' => $mineral4 ? $faker->randomNumber() : 0,
+            'mineral_4' => $mineral4 ? (object)['typeName' => $faker->randomElement($moonGooMinerals)] : (object)['typeName' => '' ],
+            'mineral_4_percent' => $mineral4 ? $faker->randomFloat(2, 5, 50) : 0,
+            'monthly_rental_fee' => $faker->numberBetween(5000000, 20000000),
+            'monthly_corp_rental_fee' => $faker->numberBetween(1000000, 5000000),
+            'previous_monthly_rental_fee' => $faker->numberBetween(5000000, 20000000),
+            'previous_monthly_corp_rental_fee' => $faker->numberBetween(1000000, 5000000),
+            'active_renter' => $faker->boolean ? (object)[
+                'character_name' => $faker->name,
+                'type' => $faker->randomElement(['individual', 'corporate']),
+                'end_date' => $faker->date(),
+                'start_date' => $faker->date(),
+            ] : null,
+            'status_flag' => $faker->randomElement([
+                \App\Models\Moon::STATUS_AVAILABLE,
+                \App\Models\Moon::STATUS_ALLIANCE_OWNED,
+                \App\Models\Moon::STATUS_LOTTERY_ONLY,
+                \App\Models\Moon::STATUS_RESERVED,
+            ]),
+            'updated_at' => $faker->dateTimeThisYear->format('Y-m-d H:i:s'),
+        ];
+
+        return (object)$data;
+    });
+
+@endphp
+
 @section('title', 'Moon Composition Data')
 
 @section('content')
@@ -7,6 +67,35 @@
     <div class="row" id="moonAdminList" data-csrf-token="{{ csrf_token() }}">
         <div class="col-12">
             <div class="card-heading">Existing Moon Data</div>
+            <form class="external-filters">
+                <!-- Region Filter (Dropdown) -->
+                <label for="region-filter">Region:</label>
+                <select id="region-filter" class="external-filter search" data-column="1">
+                    <option value="">All Regions</option>
+                    @foreach (collect($moons)->pluck('region.regionName')->unique()->sort() as $region)
+                        <option value="{{ $region }}">{{ $region }}</option>
+                    @endforeach
+                </select>
+
+                <label for="system-filter">System:</label>
+                <input type="text" id="system-filter" class="external-filter search" data-column="2" placeholder="Search System">
+
+                <label for="system-filter">Renter:</label>
+                <input type="text" id="system-filter" class="external-filter search" data-column="9" placeholder="Search Renters">
+
+                <label for="mineral-filter">Minerals:</label>
+                <input type="text" id="mineral-filter" class="external-filter search" data-column="5" placeholder="Try: Cadmium|Cobalt">
+
+                <label for="status-filter">Status:</label>
+                <select class="external-filter search" data-column="11">
+                    <option value="">All</option>
+                    <option value="Available">Available</option>
+                    <option value="Alliance owned">Alliance owned</option>
+                    <option value="Lottery only">Lottery only</option>
+                    <option value="Reserved">Reserved</option>
+                </select>
+
+            </form>
             <table id="moons">
                 <thead>
                     <tr>
@@ -94,7 +183,6 @@
 
     <script type="text/javascript">
         window.addEventListener('load', function() {
-            $('#moons').tablesorter();
             $('#moons th.moon-status-head').on('click', function () {
                 // trigger an update to sort changed values
                 $('#moons').trigger('update');
@@ -102,6 +190,16 @@
 
             $('.moon-status').each(function () {
                 setStatusText($(this));
+            });
+
+            $('#moons').tablesorter({
+                widthFixed: true,
+                widgets: ['filter'],
+                widgetOptions: {
+                    filter_columnFilters: false,
+                    filter_filteredRow: 'filtered',
+                    filter_external: '.search',
+                },
             });
         });
 
